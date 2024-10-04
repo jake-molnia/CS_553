8b058a43c2a934be2204163ce203c40db81f54db
Jacob Molnia <jrmolnia@wpi.edu>
2024-10-03 20:31:11 -0400

    inital draft for deplyment

---
+++ deployment/02_deploy_to_controller/scripts/initial_ssh_config.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ssh -i /path/to/your/private_key.pem ec2-user@paffenroth-23.dyn.wpi.edu <<EOF
+  # Change key (you need to provide the actual commands here)
+  # Install Tailscale (you need to provide the actual commands here)
+EOF
